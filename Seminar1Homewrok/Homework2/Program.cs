d01bf3e227f5d663ba441affb1a9c69442766f45
Kindelik <kindelik@yandex.ru>
2022-11-22 00:38:28 +0300

    Homework2

--- Seminar1Homewrok/Homework2/Program.cs
+++ Seminar1Homewrok/Homework2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//Примеры
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Clear();
+Console.WriteLine("Введите три числа через запятую");
+// Полученные числа загоняем в массив и преобразуем string в int
+int[] numbers = Array.ConvertAll<string,int>(Console.ReadLine().Split(','), elem => { return System.Convert.ToInt32 (elem);});
+
+Console.WriteLine($"max number {numbers.Max()} ");
