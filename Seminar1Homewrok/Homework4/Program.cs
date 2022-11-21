cea50a9621b92c1ae77d876b21f6cddd34e9ca65
Kindelik <kindelik@yandex.ru>
2022-11-22 01:17:56 +0300

    Homework4End

--- Seminar1Homewrok/Homework4/Program.cs
+++ Seminar1Homewrok/Homework4/Program.cs
@@ -2,3 +2,28 @@
 // Примеры
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int count = 2;
+if (number > 0)
+{
+    while  (count <= number )
+    {
+        Console.Write($"{count},");
+        count = count + 2;
+    }
+}
+// задача выполнена
+
+// все следующее написано, если введеное число будет отрицательным
+else
+{
+    count = 0;
+    while (count > number)
+    {
+        Console.Write($"{count},");
+        count = count -2;
+    }
+}
