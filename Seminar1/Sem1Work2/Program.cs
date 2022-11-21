b44b4ce81acc4e0b3af79c605aac5b9f22c0ef64
Kindelik <kindelik@yandex.ru>
2022-11-21 21:50:54 +0300

    Work2andWork3

---
+++ Seminar1/Sem1Work2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+//a = 25, b = 5 -> да 
+//a = 2, b = 10 -> нет 
+//a = 9, b = -3 -> да 
+//a = -3 b = 9 -> нет
+
+
+Console.Clear();
+Console.WriteLine("Введите первое целое число");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int numberB = int.Parse(Console.ReadLine());
+if(numberA == numberB * numberB)
+{
+    Console.WriteLine($" {numberA} квадрат {numberB}");
+}
+else 
+{
+ Console.WriteLine($" {numberA} не квадрат {numberB}");
+}
---
+++ Seminar1/Sem1Work3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//3 -> Среда 
+//5 -> Пятница
+
+Console.Clear()
+Console.WriteLine("Hello, User!");
+
+Console.WriteLine("Введите число дня недели");
+int numberA = int.Parse(Console.ReadLine());
+if (numberA == 1) Console.WriteLine("Понедельник");
+if (numberA == 2) Console.WriteLine("Вторник");
+if (numberA == 3) Console.WriteLine("Среда");
+if (numberA == 4) Console.WriteLine("Четверг");
+if (numberA == 5) Console.WriteLine("Пятница");
+if (numberA == 6) Console.WriteLine("Суббота");
+if (numberA == 7) Console.WriteLine("Воскресенье");
