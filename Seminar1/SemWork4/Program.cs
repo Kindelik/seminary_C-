fe29de76f625a0db0949d86c7e27c54276901f3d
Kindelik <kindelik@yandex.ru>
2022-11-21 22:50:05 +0300

    Work4andWork5

---
+++ Seminar1/SemWork4/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача №5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+//4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+//2 -> " -2, -1, 0, 1, 2"
+
+
+Console.Clear();
+Console.Write("введите число N - ");
+int numberA = int.Parse(Console.ReadLine());
+int count = 0;
+if (numberA > 0)
+{
+    count = numberA * (-1);
+    while (count < numberA + 1)
+    {
+        Console.Write($"{count}, ");
+        count++;
+    }
+}
+else
+{
+    count = numberA;
+     while (count < numberA * (-1) )
+    {
+        Console.Write($"{count}, ");
+        count++;
+    }
+}
---
+++ Seminar1/SemWork5/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+	//456 -> 6
+	//782 -> 2
+	//918 -> 8
+
+Console.Clear();
+Console.Write("введите число N - ");
+int numberA = int.Parse(Console.ReadLine());
+int count = numberA%10;
+Console.WriteLine($"endnumber {count}");
