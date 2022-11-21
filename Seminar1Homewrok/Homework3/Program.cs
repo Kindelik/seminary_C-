5ff6381a1c8b7620680831056e64214a6da40d78
Kindelik <kindelik@yandex.ru>
2022-11-22 00:53:14 +0300

    HomeWork3End

--- Seminar1Homewrok/Homework3/Program.cs
+++ Seminar1Homewrok/Homework3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//Примеры
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.Clear();
+Console.WriteLine("Enter a number");
+int number = int.Parse(Console.ReadLine());
+if(number%2 ==0) Console.WriteLine("Число четное");
+else Console.WriteLine("Число нечетное");
