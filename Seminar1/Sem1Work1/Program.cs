446d61b1b3e3f255ecc7b3a6edb8bac594fe1ffe
Kindelik <kindelik@yandex.ru>
2022-11-21 20:58:11 +0300

    Work_1_robit

--- Seminar1/Sem1Work1/Program.cs
+++ Seminar1/Sem1Work1/Program.cs
@@ -1,5 +1,9 @@
-﻿
-Console.WriteLine("Введите число");
-int numberA = Console.ReadLine();
-int numberB = numberA * numberA;
-Console.WriteLine("Квадратный корень", numberA, "=", numberB);
+﻿Console.Clear();
+Console.WriteLine("Введите число c десятичной дробью через , ");
+double numberA = double.Parse (Console.ReadLine());
+Console.WriteLine (numberA);
+double numberB = numberA * numberA;
+Console.Write("Квадратный корень ");
+Console.Write(numberA);
+Console.Write("= ");
+Console.Write(numberB);
