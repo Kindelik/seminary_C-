2f13b55bcd44c29ec043dc79791425eada6eb58e
Kindelik <kindelik@yandex.ru>
2022-11-24 21:45:14 +0300

    w4

--- Seminar2/Work4/Program.cs
+++ Seminar2/Work4/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+//7 и 23.
+
+//14 -> нет 
+//46 -> нет 
+//161 -> да
+
+Console.Clear();
+Console.WriteLine("Enter numberA");
+int numberA =int.Parse(Console.ReadLine());
+
+if ((numberA%23 ==0) && (numberA%7 ==0))
+
+{
+Console.WriteLine("кратно");
+ }
+
+ else
+ {
+ Console.WriteLine("не кратно");
+ }
