f7ef3795f685a035316817ba1213b4a15e594f84
Kindelik <kindelik@yandex.ru>
2022-12-01 00:32:40 +0300

    S3Hw2

--- Seminar3HomeWork/HomeWork2/Program.cs
+++ Seminar3HomeWork/HomeWork2/Program.cs
@@ -1,38 +1,64 @@
-﻿
-int DataЕntry(string str)                                              // Метод - DataEntry - Ввод данных формата int
+﻿// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+
+int[] DataЕntry(string str)                                              // Метод Ввод через , и перевод в массив
 
 {
     Console.WriteLine(str);
-    int number = int.Parse(Console.ReadLine());
-    return number;
+
+    int[] numbers = Array.ConvertAll<string, int>(Console.ReadLine().Split(','), elem => { return System.Convert.ToInt32(elem); });
+
+    return numbers;
 }
 
-void PrintArray(int[] array)
-{
-    int count = array.Length;
-    for (int i = 0; i < count; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
+Console.WriteLine("Enter coordinates separated by commas");     // Координаты вводить через ,
+int[] numbersA = DataЕntry("Enter the coordinates of the point A");
+int[] numbersB = DataЕntry("Enter the coordinates of the point B");
 
-Console.Clear();
+double Distance = Math.Sqrt(Math.Pow(numbersB[0] - numbersA[0], 2) + Math.Pow(numbersB[1] - numbersA[1], 2) + Math.Pow(numbersB[2] - numbersA[2], 2));
 
-int number = DataЕntry("Enter a five-digit number");
+Console.WriteLine($"Distance between points = {Distance} ");
 
-var array = number.ToString().Select(e => int.Parse(e.ToString())).ToArray();
 
-PrintArray(array);
 
-Array.Reverse(array);
 
-PrintArray(array);
 
-int[] arrayReverse = new int[array.Length];
 
-PrintArray(arrayReverse);
 
-Array.Reverse(arrayReverse);
 
-PrintArray(arrayReverse);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
