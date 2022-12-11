bc37ff56283564c1952c517be5516e01fe527035
Kindelik <kindelik@yandex.ru>
2022-12-11 22:55:21 +0300

    Seminar6Homework

---
+++ Seminar6HomeWork/Work1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+
+
+
+Console.Clear();
+Console.WriteLine("Введите числа через запятую");
+//Полученные числа загоняем в массив и преобразуем string в int
+int[] numbers =  Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+int quantity =0;  // счетчик положительных чисел
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i]>0)
+    {
+        quantity++;
+    }
+}
+System.Console.WriteLine($"Количетсво положительных чисел = {quantity}");
---
+++ Seminar6HomeWork/Work2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int DataЕntry(string str)                                              // Метод - DataEntry - Ввод данных формата int
+
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+double pointX;
+double pointY;
+
+Console.WriteLine("Даны прямые заданные формулами y = k1 * x + b1, y = k2 * x + b2");
+
+int b1 = DataЕntry("Введите значение b1");
+int k1 = DataЕntry("Введите значение k1");
+int b2 = DataЕntry("Введите значение b2");
+int k2 = DataЕntry("Введите значение k2");
+
+if (k1 == b1)
+{
+    Console.WriteLine("Прямые c такими параметрами паралельны");
+}
+else
+{
+    pointX = (double)(b2 - b1) / (k1 - k2);
+    pointY = (double)k1 * (b2 - b1) / (k1 - k2) + b1;
+    Console.WriteLine("Координаты точки пересечения двух прямых:");
+    Console.WriteLine($"Х = {pointX}");
+    Console.WriteLine($"Y = {pointY}");
+}
+
+
+/* int a = arr[1]; k1
+int b = arr[3]; k2
+int c = arr[0]; b1
+int d = arr[2]; b2
+
+if (a == b)
+{
+    Console.WriteLine("Прямые с такими параметрами паралельны");
+}
+else
+{
+    pointX = (double)(k2 - b1) / (k1 - b2);
+    pointY = (double)k1 * (k2 - b1) / (k1 - b2) + b1; */
