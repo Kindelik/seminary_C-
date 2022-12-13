04151eab17ee4a83fe42e9f6d2422289ebd204a9
Kindelik <kindelik@yandex.ru>
2022-12-14 01:58:39 +0300

    Seminar7HomeWork

---
+++ Seminar7HomeWork/Work2/Program.cs
@@ -0,0 +1,74 @@
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//            и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] TwoDimArray(int rows, int columns, int leftRange, int rigthRange)  // Метод для создания двумерного массива и заполнение его рандомными значениями
+{
+
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rigthRange + 1);
+        }
+    }
+    return array;
+}
+
+int DataЕntry(string str)                                              // Метод - DataEntry - Ввод данных формата int
+
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void SearchPosition(int position, int[,] array)  // Метод поиска числа по позиции в массиве
+{
+    int count = 0; // счетчик для перещелкивания позиции
+    int rowsPosition = 0;  // для сохранения строки искомой позиции
+    int columnsPosition = 0; // для сохранения столбца искомой позиции
+    if (position <= array.Length)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (count == position - 1)
+                {
+                    rowsPosition = i;
+                    columnsPosition = j;
+                    System.Console.WriteLine($"На позиции {position} находится число {array[rowsPosition, columnsPosition]}");
+                    System.Console.WriteLine($"Это число находится в строке {rowsPosition}, cтолбце {columnsPosition}");
+                    count++;
+                    break;
+                }
+                else
+                {
+                    count++;
+                }
+
+
+            }
+
+        }
+    }
+    else
+    {
+        System.Console.WriteLine($" Не существует введенной позиции в массиве.\n Максимальная поцизия = {array.Length}");
+    }
+}
+
+int m = 5; // количетсво строк в массиве
+int n = 5;  // количество столбцов в массиве
+int leftRange = -10;  // значение "от" целых чисел массива
+int rigthRange = 10;  // значение "до" целых чисел массива включительно
+int position = DataЕntry("Enter the item position => ");
+int[,] array = TwoDimArray(m, n, leftRange, rigthRange);   //создаем рандомный массив
+SearchPosition(position, array);  //решаем задачу с помощью этого метода
---
+++ Seminar7HomeWork/Work3/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] TwoDimArray(int rows, int columns, int leftRange, int rigthRange)  // Метод для создания двумерного массива и заполнение его рандомными значениями
+{
+
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rigthRange + 1);
+        }
+    }
+    return array;
+}
+
+double[] ArithmeticMean(int[,] array)
+{
+    double[] arrayArifmetic = new double[array.GetLength(1)];
+
+    for (int j = 0; j < array.GetLength(1); j++) // цикл для столбцов
+    {
+        for (int i = 0; i < array.GetLength(0); i++)  //цикл для строк
+        {
+            arrayArifmetic[j] += array[i, j];
+        }
+        arrayArifmetic[j] /= (double)array.GetLength(0);
+        arrayArifmetic[j] = Math.Round(arrayArifmetic[j],1); //округляем значение до десятых
+    }
+    return arrayArifmetic;
+}
+
+void PrintTwoDimArray(int[,] array)  // метод вывода двумерного массива в терминал
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void PrintArray(double[] col)             //Метод - Вывести одномерный массив в терминал
+{
+    Console.Write("[");
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.Write($" {col[i]} ");
+    }
+    Console.Write("]");
+}
+
+
+int m = 3; // количетсво строк в массиве
+int n = 4;  // количество столбцов в массиве
+int leftRange = 0;  // значение "от" целых чисел массива
+int rigthRange = 10;  // значение "до" целых чисел массива включительно
+
+int[,] array = TwoDimArray(m, n, leftRange, rigthRange);  // создаем рандомный двумерный массив
+double[] meanValues = ArithmeticMean(array); // массив, в котоырй будем складывать полученные Средние арифметические столбцов
+PrintTwoDimArray(array);  // выводим в терминал двумерный массив
+System.Console.WriteLine($"Среднее арифметическое каждого столбца {'[' + string.Join("; ", meanValues) + ']'}"); // выводим в терминал массив со средними арифметическими стоблцов
